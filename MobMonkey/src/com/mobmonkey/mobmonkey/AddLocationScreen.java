5a4d486851daf6c26aa7f799459ee26a649df00c
Arash Tadayon <arash@dezapp.com>
2013-02-22 00:04:24 -0700

    Added AddLocationScreen, fixed the History
    History now shows the most recent item first. Also if the item exists,
    moves it to the front of the list.

---
+++ MobMonkey/src/com/mobmonkey/mobmonkey/AddLocationScreen.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.mobmonkey.mobmonkey;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Dezapp LLC.
+ *
+ */
+public class AddLocationScreen extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_location_screen);
+	}
+}
