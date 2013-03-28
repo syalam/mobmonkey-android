432277087bb128485b3e2acbeaed3e3eba81aa06
Arash Tadayon <arash@dezapp.com>
2013-03-27 21:33:27 -0700

    Fixed the sub categories to show

--- MobMonkey/src/com/mobmonkey/mobmonkey/SearchScreen.java
+++ MobMonkey/src/com/mobmonkey/mobmonkey/SearchScreen.java
@@ -255,21 +255,21 @@ public class SearchScreen extends Activity implements LocationListener {
 		elvSearchCategory.setAdapter(arrayAdapter);
 		elvSearchCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			public void onItemClick(AdapterView<?> arg0, View view, int position, long arg3) {
-//				try { //TODO:Change this implementation so that it does not go to subcats
-//					String catId = topLevelCategories.getJSONObject(position).getString(MMAPIConstants.JSON_KEY_CATEGORY_ID);
-//					selectedCategory = topLevelCategories.getJSONObject(position).getString("en");
-//					JSONArray subCategories = new JSONArray(MMCategories.getSubCategoriesWithCategoriId(SearchScreen.this.getApplicationContext(), catId));
-//					
-//					if(!subCategories.isNull(0))
-//					{	
-//						Intent categoryScreenIntent = new Intent(SearchScreen.this, CategoryScreen.class);					
-//						categoryScreenIntent.putExtra(MMAPIConstants.KEY_INTENT_EXTRA_CATEGORY, (String) subCategories.toString());
-//						categoryScreenIntent.putExtra(MMAPIConstants.KEY_INTENT_EXTRA_SEARCH_RESULT_TITLE, selectedCategory);
-//						startActivity(categoryScreenIntent);
-//					}
-//				} catch (JSONException e) { 
-//					e.printStackTrace();
-//				}
+				try { //TODO:Change this implementation so that it does not go to subcats
+					//String catId = topLevelCategories.getJSONObject(position).getString(MMAPIConstants.JSON_KEY_CATEGORY_ID);
+					selectedCategory = topLevelCategories[position];
+					JSONArray subCategories = new JSONArray(MMCategories.getSubCategoriesWithCategoryName(SearchScreen.this.getApplicationContext(), selectedCategory));
+					
+					if(!subCategories.isNull(0))
+					{	
+						Intent categoryScreenIntent = new Intent(SearchScreen.this, CategoryScreen.class);					
+						categoryScreenIntent.putExtra(MMAPIConstants.KEY_INTENT_EXTRA_CATEGORY, (String) subCategories.toString());
+						categoryScreenIntent.putExtra(MMAPIConstants.KEY_INTENT_EXTRA_SEARCH_RESULT_TITLE, selectedCategory);
+						startActivity(categoryScreenIntent);
+					}
+				} catch (JSONException e) { 
+					e.printStackTrace();
+				}
 			}
 		});
 	}
