10181f67f778273c00bc07e0b1b2c9950e28cf0d
Hank Yu <hankyu@Hanks-MacBook-Air.local>
2013-04-21 17:43:50 -0700

    fixed a problem with accept/reject button when the media type is video.

--- MobMonkey/src/com/mobmonkey/mobmonkeyandroid/listeners/MMAcceptMediaOnClickListener.java
+++ MobMonkey/src/com/mobmonkey/mobmonkeyandroid/listeners/MMAcceptMediaOnClickListener.java
@@ -1,36 +1,41 @@
 package com.mobmonkey.mobmonkeyandroid.listeners;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageButton;
 
 import com.mobmonkey.mobmonkeysdk.adapters.MMMediaAdapter;
 import com.mobmonkey.mobmonkeysdk.utils.MMCallback;
+import com.mobmonkey.mobmonkeysdk.utils.MMProgressDialog;
 
 public class MMAcceptMediaOnClickListener implements OnClickListener{
 
 	private static final String TAG = "MMAcceptMediaOnClickListener";
 	private String requestId, mediaId, partnerId, emailAddress, password;
 	private MMCallback mmCallback;
+	private Context context;
 	
 	public MMAcceptMediaOnClickListener(MMCallback mmCallback, 
 										String requestId, 
 										String mediaId,
 										String partnerId,
 										String emailAddress,
-										String password) {
+										String password,
+										Context context) {
 		this.requestId = requestId;
 		this.mediaId = mediaId;
 		this.mmCallback = mmCallback;
 		this.partnerId = partnerId;
 		this.emailAddress = emailAddress;
 		this.password = password;
+		this.context = context;
 	}
 	
 	@Override
 	public void onClick(View v) {
 		Log.d(TAG, mediaId);
+		MMProgressDialog.displayDialog(context, null, "Accepting request...");
 		MMMediaAdapter.acceptMedia(mmCallback, 
 								   partnerId, 
 								   emailAddress, 
--- MobMonkey/src/com/mobmonkey/mobmonkeyandroid/listeners/MMRejectMediaOnClickListener.java
+++ MobMonkey/src/com/mobmonkey/mobmonkeyandroid/listeners/MMRejectMediaOnClickListener.java
@@ -1,34 +1,39 @@
 package com.mobmonkey.mobmonkeyandroid.listeners;
 
-import com.mobmonkey.mobmonkeysdk.adapters.MMMediaAdapter;
-import com.mobmonkey.mobmonkeysdk.utils.MMCallback;
-
+import android.content.Context;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+
+import com.mobmonkey.mobmonkeysdk.adapters.MMMediaAdapter;
+import com.mobmonkey.mobmonkeysdk.utils.MMCallback;
+import com.mobmonkey.mobmonkeysdk.utils.MMProgressDialog;
 
 public class MMRejectMediaOnClickListener implements OnClickListener{
 
 	private String requestId, mediaId, partnerId, emailAddress, password;
 	private MMCallback mmCallback;
-	private ImageButton ibtn;
+	private Context context;
 	
 	public MMRejectMediaOnClickListener(MMCallback mmCallback, 
 										String requestId, 
 										String mediaId,
 										String partnerId,
 										String emailAddress,
-										String password) {
+										String password,
+										Context context) {
 		this.requestId = requestId;
 		this.mediaId = mediaId;
 		this.mmCallback = mmCallback;
 		this.partnerId = partnerId;
 		this.emailAddress = emailAddress;
 		this.password = password;
+		this.context = context;
 	}
 	
 	@Override
 	public void onClick(View v) {
+		MMProgressDialog.displayDialog(context, null, "Rejecting request...");
 		MMMediaAdapter.rejectMedia(mmCallback, 
 								   partnerId, 
 								   emailAddress, 
@@ -36,14 +41,5 @@ public class MMRejectMediaOnClickListener implements OnClickListener{
 								   requestId, 
 								   mediaId);
 	}
-
-	public ImageButton getIbtn() {
-		return ibtn;
-	}
-
-	public void setIbtn(ImageButton ibtn) {
-		this.ibtn = ibtn;
-	}
-
 	
 }
